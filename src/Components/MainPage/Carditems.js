d7b129cbcc40ef2b927dca83c57fe43d1906fe0b
Abbas Pathan <pathan.abbas10@gmail.com>
2022-04-14 11:02:25 +0530

    Card added

---
+++ src/Components/MainPage/Carditems.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import classes from './Carditems.module.css'
+
+const Carditems = (props) => {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+    console.log(props.data.date)
+    let date = new Date(props.data.date)
+    let day = days[date.getDay()]
+    let month =months[date.getMonth()]
+
+
+  return (
+    <div className={classes.container}>
+        <button className={classes.cardbutton}>
+        <span className={classes.subhead}>{day}</span><br></br>
+        <span>{date.getDate()}</span><br></br>
+        <span className={classes.subhead}>{month}</span>
+        </button>
+    </div>
+  )
+}
+
+export default Carditems
--- src/Components/MainPage/Dashboard.js
+++ src/Components/MainPage/Dashboard.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import Carditems from "./Carditems";
+import classes from "./Dashboard.module.css";
+
+const DUMMY_DATA = [
+  {
+    date: "2022-03-22T00:00:00.000Z",
+    available: [
+      {
+        hour: 6,
+        min: 30,
+      },
+      {
+        hour: 7,
+        min: 0,
+      },
+    ],
+  },
+  {
+    date: "2022-03-23T00:00:00.000Z",
+    available: [
+      {
+        hour: 5,
+        min: 0,
+      },
+      {
+        hour: 7,
+        min: 0,
+      },
+    ],
+  },
+  {
+    date: "2022-03-24T00:00:00.000Z",
+    available: [
+      {
+        hour: 5,
+        min: 0,
+      },
+      {
+        hour: 7,
+        min: 0,
+      },
+    ],
+  },
+  {
+    date: "2022-03-25T00:00:00.000Z",
+    available: [
+      {
+        hour: 2,
+        min: 0,
+      },
+      {
+        hour: 4,
+        min: 0,
+      },
+    ],
+  },
+];
 
 const Dashboard = () => {
-  return (
-    <div></div>
-  )
-}
+  let i=0
+  const carditems = DUMMY_DATA.map((item)=>(
+    
+    <Carditems key={i++} data={item}/>
+  ))
 
-export default Dashboard
+  return (
+    <div>
+      <h3>Select Date</h3>
+      <div className={classes.cards}>
+      {carditems}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
